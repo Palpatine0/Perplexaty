7bcad0c4ef21c00339bc9cc5c463142a93fb87df
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-13 14:16:52 +0800

    feat: Integrate ExaSearchRetriever with highlights for document retrieval
    - Added `ExaSearchRetriever` with parameters `k=3` and `highlights=True`.
    - Updated main execution to retrieve and display documents with highlights.

--- perplexaty/app/chain.py
+++ perplexaty/app/chain.py
@@ -2,8 +2,20 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+from langchain_exa import ExaSearchRetriever
+
 from dotenv import load_dotenv
 from langchain.callbacks.tracers.langchain import wait_for_all_tracers
 
 load_dotenv()
 wait_for_all_tracers()
+
+retriever = ExaSearchRetriever(k = 3, highlights = True)
+documents = retriever.invoke("Best time to visit Japan")
+
+
+print(documents[0].page_content)
+
+print(documents[0].metadata["highlights"])
+
+print(documents[0].metadata["highlights"])
