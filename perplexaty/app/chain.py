e4be614391cbb9f01255dccaaad40f4b87f5e057
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-13 15:34:25 +0800

    feat: Add context-based query answering chain with ExaSearchRetriever
    - Integrated `RunnableParallel` to combine query and context processing.
    - Created a `system_template` and `human_template` for context-based research assistant responses.
    - Updated `prompt` with combined system and human templates using `ChatPromptTemplate`.
    - Formed a `chain` to process queries and contexts, generate responses, and cite sources.

--- perplexaty/app/chain.py
+++ perplexaty/app/chain.py
@@ -4,7 +4,8 @@ from langchain_core.output_parsers import StrOutputParser
 
 from langchain_exa import ExaSearchRetriever
 from langchain_core.prompts import PromptTemplate
-from langchain.schema.runnable import RunnableLambda
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough, RunnableParallel
+from langchain_core.prompts import ChatPromptTemplate
 
 from dotenv import load_dotenv
 from langchain.callbacks.tracers.langchain import wait_for_all_tracers
@@ -32,5 +33,29 @@ document_chain = RunnableLambda(
 
 retrieval_chain = retriever | document_chain.map() | (lambda docs: "\n".join([i.text for i in docs]))
 
-print(retrieval_chain.invoke("Best time to visit Japan"))
+system_template = "You are an expert research assistant. You use xml-formatted context to research people's questions."
 
+human_template = """
+Please answer the following query based on the provided context. Please cite your sources at the end of your response.
+
+Query: 
+{query}
+---
+<context>
+{context}
+</context>
+"""
+
+# Combine the system and human templates into a prompt
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_template),
+    ("human", human_template)
+])
+
+chain = RunnableParallel({
+    "query": RunnablePassthrough(),
+    "context": retrieval_chain,
+}) | prompt | ChatOpenAI()
+
+if __name__ == "__main__":
+    print(chain.invoke("Best time to visit Japan"))
